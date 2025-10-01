5810c5c3c3ea51598ae0af32d553b51ccb98ea3e
JkayJatan <jatankumarjk806@gmail.com>
2025-10-01 14:29:01 +0530

    Add responsiveness and style to contact section

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // prevent form from reloading page
 
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent form reload
+
+  const btn = document.getElementById("sendbtn");
+  btn.disabled = true;
+  btn.innerText = "Wait 5s...";
+
+  // send email with EmailJS
   emailjs.send("service_ygy4oyx", "template_5zlf0hd", {
     from_name: document.getElementById("name").value,
     from_email: document.getElementById("email").value,
     message: document.getElementById("message").value
   }).then(
-    function(response) {
+    function (response) {
       alert("Message sent successfully!");
     },
-    function(error) {
+    function (error) {
       alert("Failed to send message. Please try again.");
     }
   );
+
+  // re-enable button after 5 sec
+  setTimeout(() => {
+    btn.disabled = false;
+    btn.innerText = "Send Message";
+  }, 5000);
 });
+
